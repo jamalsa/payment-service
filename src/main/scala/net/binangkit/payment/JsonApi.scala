2cbcddb55a211bc916347979cad8c52627bcd38b
Jamal Saepul Aziz <jamal.s.aziz@gmail.com>
2015-10-15 16:30:11 +0700

    Trait for all json api.

---
+++ src/main/scala/net/binangkit/payment/JsonApi.scala
@@ -0,0 +1,30 @@
+package net.binangkit.payment
+
+import java.util.UUID
+
+import argonaut.{DecodeJson, Json}
+import argonaut.Argonaut.{jArray, jString}
+
+import org.http4s.argonaut.{jsonEncoder => defaultJsonEncoder, jsonOf}
+
+
+trait JsonApi {
+
+  implicit val jsonDecode = DecodeJson.JsonDecodeJson
+  implicit val jsonDecoder = jsonOf[Json]
+  implicit val jsonEncoder = defaultJsonEncoder
+
+  def jsonError(code: String, title: String, detail: String) = {
+    Json("errors" -> 
+      Json.array(
+        Json(
+          "code" -> jString(code),
+          "title" -> jString(title),
+          "detail" -> jString(detail)
+        )
+      )
+    )
+  }
+
+  def generateUuid = UUID.randomUUID.toString
+}
